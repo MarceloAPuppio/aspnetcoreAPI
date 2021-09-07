3e85015db8c4eb3b3a4600d6f4d7a9c5b2a75ac4
marceloapuppio <marcelopuppio@gmail.com>
2021-09-07 00:38:57 -0300

    Trabajo sobre section database

--- API/StaticFiles/scripts/index.js
+++ API/StaticFiles/scripts/index.js
@@ -1,20 +1,19 @@
 ﻿const $ = (element) => {
-    return document.querySelector(element);
-}
+  return document.querySelector(element);
+};
 
 const form = $("#form");
 const img = $("#imagen");
 
 const post = async (formdata) => {
-
-    let resp = await fetch("./Api/User", { method: "POST", body: formdata })
-    console.log(resp)
-    let obj = await resp.text()
-    console.log(obj)
-    return obj
-}
+  let resp = await fetch("./Api/User", { method: "POST", body: formdata });
+  console.log(resp);
+  let obj = await resp.text();
+  console.log(obj);
+  return obj;
+};
 
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    img.src = await post(new FormData(form))
-})
+  e.preventDefault();
+  img.src = await post(new FormData(form));
+});
